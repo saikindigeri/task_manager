124778f3bbdc57cb18c4f5b7e45ccf21f909a35c
Sai Kumar Kindigeri <132796829+saikumarkindigeriDev@users.noreply.github.com>
2024-12-26 13:03:35 +0530

    modified uri

--- server/server.js
+++ server/server.js
@@ -14,15 +14,15 @@ const connectToDatabase = async () => {
     // Use the connection string from the .env file or fallback to local MongoDB
    // const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/myDatabase';
    
-    const mongoURI = process.env.MONGO_URI || 'mongodb+srv://saikumardev018:saideva@cluster0.ngvpr.mongodb.net/myDatabase?retryWrites=true&w=majority&appName=Cluster0';
+    const mongoURI = process.env.MONGO_URI 
     await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("Connected to MongoDB"); 
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
     process.exit(1); // Exit the application if connection fails
   }
-};
-
+}; 
+ 
 connectToDatabase();
 
 // MongoDB Task Schema and Model
